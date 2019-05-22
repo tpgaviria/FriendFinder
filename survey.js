33d8a06ce504baf6a3a8e26085c9201f7d7835b7
TGAV <46203536+tpgaviria@users.noreply.github.com>
2019-05-22 12:33:19 -0400

    js

--- survey.js
+++ survey.js
@@ -1,17 +1,48 @@
-function previewFile(){
-    var preview = document.querySelector('img'); //selects the query named img
-    var file    = document.querySelector('input[type=file]').files[0]; //sames as here
-    var reader  = new FileReader();
-
-    reader.onloadend = function () {
-        preview.src = reader.result;
+$("#submit").on("click", function(event) {
+    event.preventDefault();
+    // Form validation
+    function validateForm() {
+      var isValid = true;
+      $(".form-control").each(function() {
+        if ($(this).val() === "") {
+          isValid = false;
+        }
+      });
+      $(".chosen-select").each(function() {
+        if ($(this).val() === "") {
+          isValid = false;
+        }
+      });
+      return isValid;
     }
-
-    if (file) {
-        reader.readAsDataURL(file); //reads the data as a URL
+    // If all required fields are filled
+    if (validateForm()) {
+      // Create an object for the user"s data
+      var userData = {
+        name: $("#name").val(),
+        photo: $("#photo").val(),
+        scores: [
+          $("#q1").val(),
+          $("#q2").val(),
+          $("#q3").val(),
+          $("#q4").val(),
+          $("#q5").val(),
+          $("#q6").val(),
+          $("#q7").val(),
+          $("#q8").val(),
+          $("#q9").val(),
+          $("#q10").val()
+        ]
+      };
+      // AJAX post the data to the friends API.
+      $.post("/api/friends", userData, function(data) {
+        // Grab the result from the AJAX post so that the best match's name and photo are displayed.
+        $("#match-name").text(data.name);
+        $("#match-img").attr("src", data.photo);
+        // Show the modal with the best match
+        $("#results-modal").modal("toggle");
+      });
     } else {
-        preview.src = "";
+      alert("Please fill out all fields before submitting!");
     }
-}
-
-previewFile();  //calls the function named previewFile()
+  });
